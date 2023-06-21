27f0dd571f175b24c5d843a62879c9b5009ec542
Ronnie Lawson <Quiffco@quiffco.com>
2023-06-21 16:54:16 +0100

    fix invalid key

--- keymap.c
+++ keymap.c
@@ -3,7 +3,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_split_3x5_2(
 	KC_Q, KC_W, KC_E, KC_R, KC_T,                               KC_Y, KC_U, KC_I, KC_O, KC_P,
-	KLGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), LT(2,KC_G),       LT(1,KC_H), RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),
+	LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), LT(2,KC_G),       LT(1,KC_H), RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),
 	KC_Z, KC_X, KC_C, KC_V, KC_B,                               KC_N, KC_M, KC_COMM, KC_DOT, KC_ENT,
 	                            LT(2,KC_GRV), LT(1,KC_BSPC), LT(2,KC_SPC), LT(1,KC_TAB)),
 
