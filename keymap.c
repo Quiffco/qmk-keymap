7b4d3e33c1189056a098cb12fe5484f8e64f41a7
Ronnie Lawson <Quiffco@quiffco.com>
2023-08-10 13:44:45 +0100

    Added del using backspace

--- keymap.c
+++ keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_NUBS, KC_7, KC_8, KC_9, KC_0,                            KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3,
 	KC_MINS, KC_4, KC_5, KC_6, KC_LBRC,                         KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_CAPS,
 	KC_EQL, KC_1, KC_2, KC_3, KC_RBRC,                          DT_PRNT, DT_UP, DT_DOWN, KC_NO, KC_GRV,
-	                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+	                            KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS),
 
 	[2] = LAYOUT_split_3x5_2(
 	KC_LALT, KC_F7, KC_F8, KC_F9, KC_F10,                       KC_PGUP, KC_HOME, KC_UP, KC_END, KC_NO,
