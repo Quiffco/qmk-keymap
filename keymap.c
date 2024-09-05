b5378acf79d16ccf42771d867e351e4ccfd2c99d
Ronnie Lawson <Quiffco@quiffco.com>
2024-09-05 12:15:07 +0100

    2 Tweaks
    Add mouse click to right inner thumb on layer 1
    Move Ctrl to left outer thumb and remove from home row

--- keymap.c
+++ keymap.c
@@ -3,15 +3,15 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_split_3x5_2(
 	KC_Q, KC_W, KC_E, KC_R, KC_T,				KC_Y, KC_U, KC_I, KC_O, KC_P,
-	KC_A, LALT_T(KC_S), LCTL_T(KC_D), KC_F, KC_G,		KC_H, KC_J, RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN),
+	KC_A, LALT_T(KC_S), KC_D, KC_F, KC_G,			KC_H, KC_J, KC_K, RALT_T(KC_L), RGUI_T(KC_SCLN),
 	KC_Z, KC_X, KC_C, KC_V, KC_B,				KC_N, KC_M, KC_COMM, KC_DOT, KC_ENT,
-	                            KC_LSFT, LT(1,KC_BSPC), LT(2,KC_SPC), KC_RSFT),
+	                            KC_LCTL, LT(1,KC_BSPC), LT(2,KC_SPC), KC_RSFT),
 
 	[1] = LAYOUT_split_3x5_2(
 	KC_NUBS, KC_7, KC_8, KC_9, KC_0,			KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3,
 	KC_MINS, KC_4, KC_5, KC_6, KC_LBRC,			KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_CAPS,
 	KC_EQL, KC_1, KC_2, KC_3, KC_RBRC,			DT_PRNT, DT_UP, DT_DOWN, KC_NO, KC_GRV,
-	                            KC_DEL, KC_TRNS, KC_TRNS, KC_TRNS),
+	                            KC_DEL, KC_TRNS, KC_BTN1, KC_TRNS),
 
 	[2] = LAYOUT_split_3x5_2(
 	KC_LALT, KC_F7, KC_F8, KC_F9, KC_F10,			KC_PGUP, KC_HOME, KC_UP, KC_END, KC_NO,
